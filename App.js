138d90844c173d741ea168d14575844cb5c37ad9
Abiu12 <callejas195@gmail.com>
2023-07-15 16:38:34 -0600

    feat: Configuracion navigator y enrutamiento de login

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Principal from './src/screens/Principal';
+import Login from './src/screens/Login';
+import SignUp from './src/screens/SignUp';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const Stack = createStackNavigator();
+  function MyStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Principal"
+          component={Principal}
+          options={{
+            title: "Principal",
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#8B1874" },
+            headerTintColor: "white",
+          }}
+        />
+
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: "Login",
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#8B1874" },
+            headerTintColor: "white",
+          }}
+        />
+
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{
+            title: "SignUp",
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#8B1874" },
+            headerTintColor: "white",
+          }}
+        />
+      </Stack.Navigator>
+    );
+  }
+  return(
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Login.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default function Login()  {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({})
